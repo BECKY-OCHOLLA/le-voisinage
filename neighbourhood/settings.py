a11547e423697153ac5b023f4b5f452949f39535
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-17 23:43:26 +0300

    it will add installed apps

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hood',
+    'bootstrap3',
+    'cloudinary',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
