360b0a1b42bab387786940ecc02bdbd169301fe0
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-18 00:10:58 +0300

    it will add the index view page

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from hood.models import Business, Post, Profile, Neighbourhood
 
 # Create your views here.
+def index(request):
+    # return HttpResponse('Hi there')
+    hoods = Neighbourhood.objects.all()
+    return render(request, 'index.html', {"hoods": hoods})
