ae2301402c7e3a3387959d9d95c57e3d9b3bd6ba
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-18 00:42:27 +0300

    it will create crud methods for class neighbourhood

--- hood/models.py
+++ hood/models.py
@@ -9,3 +9,25 @@ class Neighbourhood(models.Model):
     hood_description = models.TextField(max_length=500, blank=True)
     hood_photo = CloudinaryField('photo', default='photo')
     admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin')
+
+    def __str__(self):
+        return self.hood_name
+        # return f'{self.hood_name} neighbourhood'
+
+    def save_hood(self):
+        self.save()
+
+    def delete_hood(self):
+        self.delete()
+
+    @classmethod
+    def find_hood(cls, hood_id):
+        return cls.objects.filter(id=hood_id)
+
+    @property
+    def occupants_count(self):
+        return self.neighbourhood_users.count()
+
+    def update_hood(self):
+        hood_name = self.hood_name
+        self.hood_name = hood_name
