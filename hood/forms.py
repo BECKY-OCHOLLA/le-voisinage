d7f87795e2523eb3e12f995aac37d4ec029fc6bc
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-18 01:19:02 +0300

    it will make imports to forms.py

--- hood/forms.py
+++ hood/forms.py
@@ -1,3 +1,6 @@
+from hood.models import Business, Neighbourhood, Post, Profile
+from django.contrib.auth.models import User
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 
