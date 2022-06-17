6936c3ecd6b2e06480d375f511ca954cbd2458cb
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-18 00:31:05 +0300

    it will migrate models

--- hood/urls.py
+++ hood/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', ),
-    path('signup/', views.signup, name='signup'),
+    path('',views.index,name = 'index'),
+    # path('signup/', views.signup, name='signup'),
 ]
--- hood/views.py
+++ hood/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from hood.models import Business, Post, Profile, Neighbourhood
-
+from hood.models import Neighbourhood
 # Create your views here.
 def index(request):
     # return HttpResponse('Hi there')
