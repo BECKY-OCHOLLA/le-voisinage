8ddd81cd0b0a079346c1ae84e8bd998712fede97
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-18 01:00:32 +0300

    it will create forms.py

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from hood.models import Neighbourhood
+from hood.forms import *
 # Create your views here.
 def index(request):
     # return HttpResponse('Hi there')
