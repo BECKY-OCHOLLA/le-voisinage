dc38e5252d38f9be177a80efe48b7dcca6b77e7a
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-18 01:38:55 +0300

    it add crud functions and methods for class profile

--- hood/models.py
+++ hood/models.py
@@ -42,3 +42,22 @@ class Profile(models.Model):
     profile_pic = CloudinaryField('profile')
     bio = models.TextField(max_length=500, blank=True)
     neighbourhood = models.ForeignKey( Neighbourhood, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(cls, id):
+        Profile.objects.get(user_id=id)
+
