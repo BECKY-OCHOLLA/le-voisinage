cd41879bcc5fa76cc0efcc18f5c892a62c439b97
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-18 01:02:46 +0300

    it will create signupform

---
+++ hood/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
