f379c4890630326f53506109e3793cb9ef471b56
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-18 23:29:17 +0300

    it will add the join hood,leavehood,view hood,new biz,view biz views

--- hood/views.py
+++ hood/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render,redirect
+from hood.models import Business, Post, Profile, Neighbourhood
 from django.contrib.auth.models import User
-from hood.models import Neighbourhood
-from hood.forms import *
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render, redirect
+from hood.forms import NewBusinessForm, NewHoodForm, PostForm, SignUpForm, UpdateProfileForm, UpdateUserForm
 from django.http.response import HttpResponse, HttpResponseRedirect
-
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 
 
 # Create your views here.
@@ -55,6 +54,79 @@ def update_profile(request, id):
 
 
 
+@login_required(login_url='/accounts/login/')
+def hood(request):
+    # current_user = request.user
+    if request.method == 'POST':
+        form = NewHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user
+
+            hood.save()
+
+        return redirect('index')
+
+    else:
+        form = NewHoodForm()
+    return render(request, 'newhood.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def joinhood(request, id):
+    hood = get_object_or_404(Neighbourhood, id=id)
+    request.user.profile.neighbourhood = hood
+    request.user.profile.save()
+    return redirect('index')
+
+def leavehood(request, id):
+    hood = get_object_or_404(Neighbourhood, id=id)
+    request.user.profile.neighbourhood = None
+    request.user.profile.save()
+    return redirect('index')
+
+@login_required(login_url='/accounts/login')
+def view_hood(request, id):
+    hood = Neighbourhood.objects.get(id=id)
+    biz = Business.objects.filter(business_hood=id)
+    post = Post.objects.filter(neighbourhood=id)
+
+    # business_hood
+    
+    # biz_available = None
+    # if biz is None:
+    #     biz_available = False
+    # else:
+    #     biz_available = True
+
+    current_user = request.user
+    return render(request, 'view_hood.html',  {
+        'hood': hood,'business':biz,'post': post
+    })
+
+@login_required(login_url='/accounts/login/')
+def new_business(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewBusinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            biz = form.save(commit=False)
+            biz.user = current_user
+
+            biz.save()
+
+        return redirect('index')
+
+    else:
+        form = NewBusinessForm()
+    return render(request, 'new_business.html', {"form": form})
+
+@login_required(login_url='/accounts/login')
+def view_biz(request, id):
+    biz = Business.objects.get(id=id)
+    return render(request, {'business':biz,
+    })
+
+@login_required(login_url='/accounts/login/')
 def search(request):
     if 'business' in request.GET and request.GET['business']:
         business = request.GET.get("business")
@@ -65,4 +137,19 @@ def search(request):
         message = "You haven't searched for anything, please try again"
     return render(request, 'search.html', {'message': message})
 
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
 
+            post.save()
+
+        return redirect('index')
+
+    else:
+        form = PostForm()
+    return render(request, 'new_post.html', {"form": form})
