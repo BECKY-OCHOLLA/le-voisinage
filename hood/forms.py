7563233ed6e237042fb173c363b8362b686caf14
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-06-18 12:43:02 +0300

    it add UpdateUser Form and UpdateProfileForm

--- hood/forms.py
+++ hood/forms.py
@@ -1,4 +1,4 @@
-from hood.models import  Neighbourhood,  Profile
+from hood.models import Business, Neighbourhood, Post, Profile
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
@@ -10,3 +10,17 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+
+class UpdateUserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('username',)
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['email','profile_pic', 'bio','neighbourhood']
+
+
